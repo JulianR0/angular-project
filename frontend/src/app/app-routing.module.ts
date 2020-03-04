bbced9c63453783a76f745fc58837d6630ac043a
Julian <jumarti_5@yahoo.com.ar>
2020-03-03 22:09:24 -0300

    Temporarily removed most components for reworks.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMenuComponent } from './mainmenu/mainmenu.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
-import { TeamManagerComponent } from './teammanager/teammanager.component';
+//import { PokemonComponent } from './pokemon/pokemon.component';
+//import { TeamManagerComponent } from './teammanager/teammanager.component';
 import { InternalPKMNManagerComponent } from './internal-pkmnmanager/internal-pkmnmanager.component';
-import { InternalMoveManagerComponent } from './internal-movemanager/internal-movemanager.component';
-import { BattleComponent } from './battle/battle.component';
+//import { InternalMoveManagerComponent } from './internal-movemanager/internal-movemanager.component';
+//import { BattleComponent } from './battle/battle.component';
 
 const routes: Routes =
 [
-	{ path: 'team', component: TeamManagerComponent },
-	{ path: 'team/:teamID', component: PokemonComponent },
-	{ path: 'battle', component: BattleComponent },
+	//{ path: 'team', component: TeamManagerComponent },
+	//{ path: 'team/:teamID', component: PokemonComponent },
+	//{ path: 'battle', component: BattleComponent },
 	{ path: 'i_pokemon', component: InternalPKMNManagerComponent },
-	{ path: 'i_moves', component: InternalMoveManagerComponent },
+	//{ path: 'i_moves', component: InternalMoveManagerComponent },
 	{ path: 'main', component: MainMenuComponent },
 	{ path: '', redirectTo: '/main', pathMatch: 'full' }
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,30 +5,31 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
+//import { PokemonComponent } from './pokemon/pokemon.component';
 import { PopupsComponent } from './popups/popups.component';
 import { MainMenuComponent } from './mainmenu/mainmenu.component';
-import { TeamManagerComponent } from './teammanager/teammanager.component';
+//import { TeamManagerComponent } from './teammanager/teammanager.component';
 import { InternalPKMNManagerComponent } from './internal-pkmnmanager/internal-pkmnmanager.component';
-import { InternalMoveManagerComponent } from './internal-movemanager/internal-movemanager.component';
-import { BattleComponent } from './battle/battle.component';
+//import { InternalMoveManagerComponent } from './internal-movemanager/internal-movemanager.component';
+//import { BattleComponent } from './battle/battle.component';
 
 @NgModule
 ({
 	declarations:
 	[
 		AppComponent,
-		PokemonComponent,
+		//PokemonComponent,
 		PopupsComponent,
 		MainMenuComponent,
-		TeamManagerComponent,
+		//TeamManagerComponent,
 		InternalPKMNManagerComponent,
-		InternalMoveManagerComponent,
-		BattleComponent
+		//InternalMoveManagerComponent,
+		//BattleComponent
 	],
 	imports:
 	[
@@ -38,6 +39,7 @@ import { BattleComponent } from './battle/battle.component';
 		MatDialogModule,
 		MatInputModule,
 		MatSelectModule,
+		DragDropModule,
 		FormsModule,
 		ReactiveFormsModule,
 		AppRoutingModule
